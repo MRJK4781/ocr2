d14fd8601647d31a37121fde3ed4bf251dfe6a67
MRJK4781 <124258352+MRJK4781@users.noreply.github.com>
2023-12-05 23:51:00 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -10,25 +10,18 @@ Welcome to {}
 
 I can extract text from images using OCR technology.
 
-By @StarkBots
     """
 
     # Home Button
     home_buttons = [
-        [InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")],
         [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")],
     ]
     # Rest Buttons
     buttons = [
         [
-            InlineKeyboardButton("✨ Bot Status and More Bots ✨", url="https://t.me/StarkBots/7")
-        ],
-        [
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
             InlineKeyboardButton("🎪 About 🎪", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
     ]
 
     # Help Message
@@ -38,21 +31,14 @@ You Really Need Help ?!?!?!?!
 Just send an image. Rest is on me.
 
 Note : You can send any amount of images at once and it will work with same speed and accuracy.
-
-More features in development. Keep track by joining @StarkBots.
     """
 
     # About Message
     ABOUT = """
-**About This Bot** 
-
-Bot created by @StarkBots
-
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/OCRBot)
+**About This Bot**  
 
 Framework : [Pyrogram](docs.pyrogram.org)
 
 Language : [Python](www.python.org)
 
-Developer : @StarkProgrammer
     """
